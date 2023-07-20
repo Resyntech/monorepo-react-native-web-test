cabd3da895626f4021ddc0211f66dd4bf84f7121
Resyntech <carranzagcarlo@gmail.com>
2023-07-20 11:27:49 +0800

    issues in README

---
+++ packages/test/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import Home from "./src/screens";
+
+const App = () => {
+  return (
+    <ScrollView>
+      <Home />
+    </ScrollView>
+  );
+};
+
+export default App;
---
+++ packages/test/metro.config.js
@@ -0,0 +1,29 @@
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const path = require("path");
+
+const watchFolders = [
+  //Relative path to packages directory
+  path.resolve(__dirname + "/../../node_modules"), // References `yantracker-test/node_modules`
+  path.resolve(__dirname + "/../../node_modules/react-native"), // References `react-native`
+  path.resolve(__dirname + "/../../node_modules/nativewind/tailwind"), // References `css`
+];
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  watchFolders,
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
